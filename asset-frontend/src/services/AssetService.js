bca7a0929a98ee8d81bb42c9c10374061487ee22
Sohel <smssohel.777@gmail.com>
2026-05-24 02:05:28 +0530

    Updated frontend API URL

--- asset-frontend/src/services/AssetService.js
+++ asset-frontend/src/services/AssetService.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/assets";
+const API_URL = "https://javafullstack-asset-manager.onrender.com/api/assets";
 
-export const getAssets = () => {
-  return axios.get(API_URL);
-};
+export const getAssets = () => axios.get(API_URL);
 
-export const addAsset = (asset) => {
-  return axios.post(API_URL, asset);
-};
+export const addAsset = (asset) => axios.post(API_URL, asset);
 
-export const deleteAsset = (id) => {
-  return axios.delete(`${API_URL}/${id}`);
-};
+export const deleteAsset = (id) =>
+  axios.delete(`${API_URL}/${id}`);
+
+export const updateAsset = (id, asset) =>
+  axios.put(`${API_URL}/${id}`, asset);
