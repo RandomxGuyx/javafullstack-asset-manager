40b0b676a5066c055b3ca3312cc4b5ab4fdb3cce
Sohel <smssohel.777@gmail.com>
2026-05-24 03:37:55 +0530

    Fixed frontend API connection

--- asset-frontend/src/services/AssetService.js
+++ asset-frontend/src/services/AssetService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://javafullstack-asset-manager.onrender.com/api/assets";
+const API_URL = "https://javafullstack-asset-manager.onrender.com/assets";
 
 export const getAssets = () => axios.get(API_URL);
 
